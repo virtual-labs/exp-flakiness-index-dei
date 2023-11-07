52d306cea4fd964100bb6a9678fabbfe0983de80
Varun Pal Singh <singhvarun928@gmail.com>
2023-11-07 13:45:50 +0530

    convert codebase from monolith to cuncks of steps

---
+++ experiment/simulation/js/step8.js
@@ -0,0 +1,14 @@
+// Step 8
+
+const step8 = document.querySelector("#step8");
+const instructionsStep8 = document.querySelector("#instructionsStep8");
+const conclusion = document.querySelector("#conclusion");
+const arrNext = document.querySelector(".arr-next");
+
+// images of step8
+const png1Step8 = document.querySelector("#png1Step8");
+addEventListener("DOMContentLoaded", (evt) =>{
+    step8.style.visibility = "visible";
+    png1Step8.style.visibility = "visible";
+    instructionsStep8.textContent = "Result"
+});
